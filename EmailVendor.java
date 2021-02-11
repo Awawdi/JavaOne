8972fee5b02a2cdc019d9e947d515b36e1df8f51
Orsan Awawdi <orsan.awawdi@gmail.com>
2021-02-11 18:59:09 +0200

    added Employee file

--- EmailVendor.java
+++ EmailVendor.java
@@ -4,6 +4,8 @@ public class EmailVendor implements Vendors
 
         private String serverAddress;
         private String postFix;
+        private String preFix;
+
 
         private final static String userName = "admin";
         private final static String password = "admin";
@@ -12,10 +14,11 @@ public class EmailVendor implements Vendors
         {
             this.name = VendorName;
         }
-        public void managePreferences(String serverAddress,String postFix)
+        public void managePreferences(String serverAddress,String postFix, Employee Emp)
         {
             this.serverAddress = serverAddress;
             this.postFix = postFix;
+            this.preFix= Emp.Name;
         }
 
         public String getserverAddress()
@@ -29,7 +32,7 @@ public class EmailVendor implements Vendors
             System.out.println(recipient);
             System.out.println("using the following info:");
             System.out.println("SMTP: " + this.serverAddress);
-            System.out.println("From: " + name + this.postFix);
+            System.out.println("From: " + this.preFix + this.postFix);
             System.out.println("SMTP user: " + userName);
             System.out.println("SMTP pass: " + password);
             
@@ -41,24 +44,5 @@ public class EmailVendor implements Vendors
 
         
     
-    public static void main(String[] args)
-    {
-        System.out.println("Lets do it!");
-
-        EmailVendor Gmail = new EmailVendor("gmail");
-        Gmail.managePreferences("smtp.gmail.com", "@gmail.com");
-        System.out.println(Gmail.getserverAddress());
-
-        EmailVendor Walla = new EmailVendor("walla");
-        Walla.managePreferences("smtp.walla.co.il", "@walla.co.il");
-        System.out.println(Walla.getserverAddress());
-
-        EmailVendor Yahoo = new EmailVendor("yahoo");
-        Yahoo.managePreferences("smtp.yahoo.com", "@yahoo.com");
-        System.out.println(Yahoo.getserverAddress());
-
-        Yahoo.sendEmail("orsan.awawdi@gmail.com");
-
-
-    }
+    
 }
