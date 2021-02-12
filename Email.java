0b43c34435e35d6bb5ee27ae097a6e5cdfe5610c
Orsan Awawdi <orsan.awawdi@gmail.com>
2021-02-12 22:09:57 +0200

    added email class and few fields

---
+++ Email.java
@@ -0,0 +1,8 @@
+public class Email {
+    public Employee Sender;
+    public Employee Receipt;
+    public EmailVendor Vendor;
+    public EmailVendor To;
+    public EmailVendor From;
+    public String Body;    
+}
